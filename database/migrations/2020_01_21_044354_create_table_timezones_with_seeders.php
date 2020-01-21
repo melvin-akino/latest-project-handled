ded6e018fdfb86fcb8006d808593d69fc08e4ee1
I Am Groot <kevinuy@ninepinetech.com>
2020-01-21 14:16:06 +0800

    Code Rearrange - `database/migrations/create_table_timezones`
    Minor code adjustments

--- database/migrations/2020_01_21_044354_create_table_timezones_with_seeders.php
+++ database/migrations/2020_01_21_044354_create_table_timezones_with_seeders.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\{Artisan, Schema};
 
 class CreateTableTimezonesWithSeeders extends Migration
 {
