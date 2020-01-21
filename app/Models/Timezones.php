0b393543a03ff081efb89590861d5cfe98a8df69
I Am Groot <kevinuy@ninepinetech.com>
2020-01-21 13:49:06 +0800

    Code Rearrange - app\Models\Timezones
    Fixed Code Alignment from file `app\Models\Timezones`

--- app/Models/Timezones.php
+++ app/Models/Timezones.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Timezones extends Model
 {
-	protected $table = "timezones";
+    protected $table = "timezones";
 
     protected $fillable = [
     	'name',
