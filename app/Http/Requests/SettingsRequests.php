8adcc5f32c1335d3a92aff1822ca13aa4ea981ee
I Am Groot <kevinuy@ninepinetech.com>
2020-01-23 15:59:41 +0800

    API - User Settings - Backend Validations
    - Added Laravel Request Validations for User Settings

--- app/Http/Requests/SettingsRequests.php
+++ app/Http/Requests/SettingsRequests.php
@@ -27,9 +27,62 @@ class SettingsRequests extends FormRequest
     {
         $type = $this->route('type');
 
-        return [
-            //
-        ];
+        if ($type == 'general') {
+            $priceFormats = implode(',', array_column(config('constants.price-format'), 'id'));
+
+            return [
+                'price_format'  => 'required|numeric|in:' . $priceFormats,
+                'timezone'      => 'required|numeric|exists:timezones,id',
+            ];
+        } else if ($type == 'trade-page') {
+            $tradeLayouts = implode(',', array_column(config('constants.trade-layout'), 'id'));
+            $sortEvents = implode(',', array_column(config('constants.sort-event'), 'id'));
+
+            return [
+                'suggested'                 => 'required|boolean',
+                'trade_background'          => 'required|boolean',
+                'hide_comp_names_in_fav'    => 'required|boolean',
+                'live_position_values'      => 'required|boolean',
+                'hide_exchange_only'        => 'required|boolean',
+                'trade_layout'              => 'required|numeric|in:' . $tradeLayouts,
+                'sort_event'                => 'required|numeric|in:' . $sortEvents,
+            ];
+        } else if ($type == 'bet-slip') {
+            $selections = implode(',', array_column(config('constants.betslip-adaptive-selection'), 'id'));
+
+            return [
+                'use_equivalent_bets'   => 'required|boolean',
+                'offers_on_exchanges'   => 'required|boolean',
+                'adv_placement_opt'     => 'required|boolean',
+                'bets_to_fav'           => 'required|boolean',
+                'adv_betslip_info'      => 'required|boolean',
+                'tint_bookies'          => 'required|boolean',
+                'adaptive_selection'    => 'required|numeric|in:' . $selections,
+            ];
+        } else if ($type == 'bookies') {
+            return [
+                // 'disabled_bookies'      => 'array',
+            ];
+        } else if ($type == 'bet-columns') {
+            return [
+                // 'disabled_columns'      => 'array',
+            ];
+        } else if ($type == 'notifications-and-sounds') {
+            return [
+                'bet_confirm'           => 'required|boolean',
+                'site_notifications'    => 'required|boolean',
+                'popup_notifications'   => 'required|boolean',
+                'order_notifications'   => 'required|boolean',
+                'event_sounds'          => 'required|boolean',
+                'order_sounds'          => 'required|boolean',
+            ];
+        } else if ($type == 'language') {
+            $languages = implode(',', array_column(config('default_config.language.languages'), 'id'));
+
+            return [
+                'language'              => 'required|numeric|in:' . $languages,
+            ];
+        }
     }
 
     /**
