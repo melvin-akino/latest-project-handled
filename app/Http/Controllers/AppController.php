c7d7c53d09f420bdf5b0f266003e9c5d62767d84
Xander <johnvirtucio@ninepinetech.com>
2020-01-27 18:13:34 +0800

    MV-155 move config list to controller

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AppController extends Controller
 {
     public function index()
     {
-        return view('app');
+        $defaultData = [
+            'price-format'               => config('constants.price-format'),
+            'trade-layout'               => config('constants.trade-layout'),
+            'sort-event'                 => config('constants.sort-event'),
+            'betslip-adaptive-selection' => config('constants.betslip-adaptive-selection'),
+            'language'                   => config('constants.language'),
+        ];
+
+
+        return view('app', [
+            'default_data' => $defaultData
+        ]);
     }
 }
