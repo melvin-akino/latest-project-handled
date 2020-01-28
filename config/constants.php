0d9d57cefa05cbe2326880f2f3bd9853ad24787c
Xander <johnvirtucio@ninepinetech.com>
2020-01-28 15:01:35 +0800

    MV-202 added language list

--- config/constants.php
+++ config/constants.php
@@ -5,13 +5,16 @@ return [
         [
             'id'    => 1,
             'value' => 'Decimal',
-        ], [
+        ],
+        [
             'id'    => 2,
             'value' => 'HongKong',
-        ], [
+        ],
+        [
             'id'    => 3,
             'value' => 'United States',
-        ], [
+        ],
+        [
             'id'    => 4,
             'value' => 'Indo Odds',
         ],
@@ -20,7 +23,8 @@ return [
         [
             'id'    => 1,
             'value' => 'Asian',
-        ], [
+        ],
+        [
             'id'    => 2,
             'value' => 'European',
         ],
@@ -29,7 +33,8 @@ return [
         [
             'id'    => 1,
             'value' => 'Competition Name',
-        ], [
+        ],
+        [
             'id'    => 2,
             'value' => 'Event Start Time',
         ],
@@ -38,12 +43,26 @@ return [
         [
             'id'    => 1,
             'value' => "Never",
-        ], [
+        ],
+        [
             'id'    => 2,
             'value' => "Only bookies at placement",
-        ], [
+        ],
+        [
             'id'    => 3,
             'value' => "Add new bookies to order",
+        ],
+    ],
+    'language'                   => [
+        [
+            'id'    => 1,
+            'key'   => "en",
+            'value' => "English",
+        ],
+        [
+            'id'    => 2,
+            'key'   => "es",
+            'value' => "Spanish",
         ],
     ],
 ];
