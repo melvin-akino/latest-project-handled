fa1b9eb2753e657de5f24e1f8a826e244204f543
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-01-31 14:31:12 +0800

    MV-224
    Email displaying on textbox for reset password

--- resources/js/components/views/auth/ResetPassword.vue
+++ resources/js/components/views/auth/ResetPassword.vue
@@ -48,7 +48,7 @@ export default {
     data() {
         return {
             resetPasswordForm: {
-                email: '',
+                email: this.$store.state.auth.resetPasswordEmail,
                 password: '',
                 password_confirmation: '',
             },
