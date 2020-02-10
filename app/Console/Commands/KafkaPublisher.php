b7e853a2f58bef1e90e8c45c7c53975c7ae0495c
Xander <johnvirtucio@ninepinetech.com>
2020-02-10 11:54:21 +0800

    Test publish

--- app/Console/Commands/KafkaPublisher.php
+++ app/Console/Commands/KafkaPublisher.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Log;
 use RdKafka\Conf as KafkaConf;
 use RdKafka\KafkaConsumer;
+use RdKafka\Producer;
 use RdKafka\Message;
 use SwooleTW\Http\Websocket\Facades\Websocket;
 use swoole_websocket_server;
@@ -22,7 +23,7 @@ class KafkaPublisher extends Command
      *
      * @var string
      */
-    protected $signature = 'kafka:publish';
+    protected $signature = 'kafka:publish {message}';
 
     /**
      * The console command description.
@@ -48,6 +49,32 @@ class KafkaPublisher extends Command
      */
     public function handle()
     {
-// do your process
+        $message = $this->argument('message');
+        $kafkaProducer = new Producer($this->getConfig());
+
+        $topic = $kafkaProducer->newTopic('alex-scraping');
+        $topic->produce(RD_KAFKA_PARTITION_UA, 0, $message);
+    }
+
+    protected function getConfig()
+    {
+        $conf = new KafkaConf();
+
+        // Configure the group.id. All consumer with the same group.id will consume
+        // different partitions.
+        $conf->set('group.id', 'multiline');
+
+        // Initial list of Kafka brokers
+        $conf->set('metadata.broker.list', env('KAFKA_BROKERS', 'kafka:9092'));
+
+        // Set where to start consuming messages when there is no initial offset in
+        // offset store or the desired offset is out of range.
+        // 'smallest': start from the beginning
+        $conf->set('auto.offset.reset', 'smallest');
+
+        // Automatically and periodically commit offsets in the background
+        $conf->set('enable.auto.commit', 'false');
+
+        return $conf;
     }
 }
