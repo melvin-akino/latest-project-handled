6781271b2f5c85475fb9f5d708e02fed2b29b434
I Am Groot <kevinuy@ninepinetech.com>
2020-02-11 18:15:02 +0800

    API - User Watchlist Endpoints
    Created API Endpoints for Fetching, Adding, and Removing Game Events to User Watchlist

---
+++ resources/lang/en/game.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'watchlist' => [
+        'success'   => "Game Successfully Added to your Watchlist",
+        'removed'   => "Game Successfully Removed to your Watchlist",
+        'failed'    => "Something went wrong. Please try again.",
+        'not-found' => "Sorry, that game does not exist anymore.",
+    ],
+];
