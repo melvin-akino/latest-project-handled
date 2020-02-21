4cca7caf6a7ebf32a9c6c65349212de8a1b4be01
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-02-21 13:02:23 +0800

    MV-362
    Adjusted sports dropdown and leagues list

--- resources/js/components/views/trade/Sports.vue
+++ resources/js/components/views/trade/Sports.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="text-white mb-2 pl-4 shadow-xl">
         <div class="flex flex-col overflow-hidden">
-            <div class="text-center text-white text-sm cursor-pointer py-2 bg-orange-500">Sports</div>
             <div class="sports overflow-y-auto flex flex-col bg-white text-gray-700">
-                <div class="sport" v-for="sport in sports" :key="sport.id">
-                    <div class="flex items-center w-full text-left text-sm py-1 px-6"  :class="[selectedSport === sport.id ? 'bg-gray-900 text-white' : '',  { 'text-gray-600' : !sport.is_enabled }]" >
-                        <i class="material-icons sportsIcon pr-2">{{sport.icon}}</i>
-                        <button type="button" class="focus:outline-none"@click="selectSport(sport.id)" :disabled="!sport.is_enabled"><span class="pb-1">{{sport.sport}}</span></button>
+                <div class="sport" v-for="(sport, index) in sportsList" :key="sport.id">
+                    <div class="flex text-left text-sm py-1 px-6"  :class="[selectedSport === sport.id ? 'bg-gray-900 text-white' : '',  { 'text-gray-600' : !sport.is_enabled }]" >
+                        <button type="button" class="flex justify-between items-center w-full focus:outline-none" @click="selectSport(sport.id)" :disabled="!sport.is_enabled">
+                            <div class="sportBtn">
+                                <i class="material-icons sportsIcon pr-2">{{sport.icon}}</i>
+                                <span>{{sport.sport}}</span>
+                            </div>
+                            <div class="sportsListToggle" v-if="index===0">
+                                <span v-show="isSportsListOpen"><i class="fas fa-chevron-down"></i></span>
+                                <span v-show="!isSportsListOpen"><i class="fas fa-chevron-up"></i></span>
+                            </div>
+                        </button>
                     </div>
-                    <div class="leagues" v-if="selectedSport === sport.id">
+                    <div class="leagues" v-if="!isSportsListOpen">
                         <Leagues></Leagues>
                     </div>
                 </div>
@@ -26,13 +33,25 @@ export default {
     data() {
         return {
             sports: [],
+            leagues: {},
+            isSportsListOpen: false
         }
     },
     components: {
         Leagues
     },
     computed: {
-        ...mapState('trade', ['selectedSport'])
+        ...mapState('trade', ['selectedSport']),
+        sportsList() {
+            if(this.isSportsListOpen) {
+                let sports = this.sports.filter(sport => sport.id != this.selectedSport)
+                let selectedSport = this.sports.filter(sport => sport.id === this.selectedSport)[0]
+                sports.unshift(selectedSport)
+                return sports
+            } else {
+                return this.sports.filter(sport => sport.id === this.selectedSport)
+            }
+        }
     },
     mounted() {
         this.$store.commit('trade/SET_SELECTED_SPORT', 1)
@@ -41,6 +60,7 @@ export default {
     methods: {
         selectSport(sport) {
             this.$store.commit('trade/SET_SELECTED_SPORT', sport)
+            this.isSportsListOpen = !this.isSportsListOpen
         },
         getSports() {
             let token = Cookies.get('mltoken')
@@ -57,7 +77,8 @@ export default {
 
 <style>
     .sports {
-        max-height:420px;
+        max-height:440px;
+        overflow-y:auto;
     }
 
     .sportsIcon {
