6ba456a2a1a7c1b74fd9c50ebf1109273a0cd147
Rey Dorado <reynaldodorado@ninepinetech.com>
2020-02-25 01:02:49 +0800

    Refactor: create kafka producer handle

---
+++ config/scraping.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    'schedule_mapping' => [
+        'inplay' => [
+            'SCHEDULE_INPLAY_TIMER',
+            'INTERVAL_REQ_PER_EXEC_INPLAY',
+            'NUM_OF_REQ_PER_EXECUTION_INPLAY',
+        ],
+        'today'  => [
+            'SCHEDULE_TODAY_TIMER',
+            'INTERVAL_REQ_PER_EXEC_TODAY',
+            'NUM_OF_REQ_PER_EXECUTION_TODAY',
+        ],
+        'early'  => [
+            'SCHEDULE_EARLY_TIMER',
+            'INTERVAL_REQ_PER_EXEC_EARLY',
+            'NUM_OF_REQ_PER_EXECUTION_EARLY',
+        ]
+    ],
+
+    'schedule_mapping_field' => [
+        'SCHEDULE_INPLAY_TIMER' => 'timer',
+        'INTERVAL_REQ_PER_EXEC_INPLAY' => 'requestInterval',
+        'NUM_OF_REQ_PER_EXECUTION_INPLAY' => 'requestNumber',
+        'SCHEDULE_TODAY_TIMER' => 'timer',
+        'INTERVAL_REQ_PER_EXEC_TODAY' => 'requestInterval',
+        'NUM_OF_REQ_PER_EXECUTION_TODAY' => 'requestNumber',
+        'SCHEDULE_EARLY_TIMER' => 'timer',
+        'INTERVAL_REQ_PER_EXEC_EARLY' => 'requestInterval',
+        'NUM_OF_REQ_PER_EXECUTION_EARLY' => 'requestNumber',
+    ],
+
+    'refresh_db_interval' => 60 * 10,
+
+];
