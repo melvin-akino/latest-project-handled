51749520bcd639d5d0f33757aad95b27773ae9f1
Rey Dorado <reynaldodorado@ninepinetech.com>
2020-02-26 10:09:47 +0800

    add comment

--- config/scraping.php
+++ config/scraping.php
@@ -2,22 +2,29 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Scraping timing system config 
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
     'schedule_mapping' => [
         'inplay' => [
             'SCHEDULE_INPLAY_TIMER',
             'INTERVAL_REQ_PER_EXEC_INPLAY',
             'NUM_OF_REQ_PER_EXECUTION_INPLAY',
         ],
-        'today'  => [
+        'today' => [
             'SCHEDULE_TODAY_TIMER',
             'INTERVAL_REQ_PER_EXEC_TODAY',
             'NUM_OF_REQ_PER_EXECUTION_TODAY',
         ],
-        'early'  => [
+        'early' => [
             'SCHEDULE_EARLY_TIMER',
             'INTERVAL_REQ_PER_EXEC_EARLY',
             'NUM_OF_REQ_PER_EXECUTION_EARLY',
-        ]
+        ],
     ],
 
     'schedule_mapping_field' => [
