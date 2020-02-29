4bbe3842001cf1ecf4b303e337ea37463d29ed98
Xander <johnvirtucio@ninepinetech.com>
2020-02-29 13:04:02 +0800

    MV-407 rollback update

--- database/migrations/2020_02_27_162034_update_user_selected_leagues.php
+++ database/migrations/2020_02_27_162034_update_user_selected_leagues.php
@@ -19,7 +19,7 @@ class UpdateUserSelectedLeagues extends Migration
         if (Schema::hasTable($this->tableName)) {
             Schema::table($this->tableName, function (Blueprint $table) {
                 $table->dropColumn('master_league_id');
-                $table->string('master_league_name', 30)->default('');
+                $table->string('master_league_name', 100)->default('');
             });
         }
     }
