5b02f5b033d46149eec80a850f054d5f01c33187
Xander <johnvirtucio@ninepinetech.com>
2020-03-12 10:32:33 +0800

    Masterlist console

---
+++ app/Models/MasterTeam.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MasterTeam extends Model
+{
+    use SoftDeletes;
+
+    protected $table = "master_teams";
+
+    protected $fillable = [
+        'sport_id',
+        'master_team_name',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+}
