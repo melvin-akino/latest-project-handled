2d798201d3901e70f74c4a6d1001f484bdbc66ff
Xander <johnvirtucio@ninepinetech.com>
2020-03-12 19:53:23 +0800

    MV-484 Initial Min Max Producer

--- config/laravels.php
+++ config/laravels.php
@@ -37,6 +37,12 @@ return [
             'pipe' => 0,
             'enable' => env('LARAVELS_KAFKA_CONSUME', true)
         ],
+        'kafka_produce' => [
+            'class'    => \App\Processes\KafkaProduce::class,
+            'redirect' => false,
+            'pipe' => 0,
+            'enable' => env('LARAVELS_KAFKA_PRODUCE', true)
+        ],
     ],
     'timer'                    => [
         'enable'        => env('LARAVELS_TIMER', false),
@@ -221,7 +227,7 @@ return [
         'reactor_num'        => env('LARAVELS_REACTOR_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() * 2 : 4),
         'worker_num'         => env('LARAVELS_WORKER_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() * 2 : 8),
         'task_worker_num'    => env('LARAVELS_TASK_WORKER_NUM', function_exists('swoole_cpu_num') ? swoole_cpu_num() * 2 : 8),
-        'task_ipc_mode'      => 2,
+        'task_ipc_mode'      => 1,
         'task_max_request'   => env('LARAVELS_TASK_MAX_REQUEST', 8000),
         'task_tmpdir'        => @is_writable('/dev/shm/') ? '/dev/shm' : '/tmp',
         'max_request'        => env('LARAVELS_MAX_REQUEST', 8000),
