15970d08fa7d4e71dcafffedd9932841b1382afd
Alex <johnvirtucio@ninepinetech.com>
2020-03-16 13:39:26 +0800

    MV-489 socket receive order command from UI client

---
+++ app/Jobs/WsOrder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class WsOrder implements ShouldQueue
+{
+    use Dispatchable;
+
+    public function __construct($userId, $params)
+    {
+        $this->userId  = $userId;
+        $this->orderId = $params[1];
+    }
+
+    public function handle()
+    {
+        $topicTable = app('swoole')->topicTable;
+        $topicTable->set('userId:' . $this->userId . ':unique:' . uniqid(), [
+            'user_id'    => $this->userId,
+            'topic_name' => 'order-' . $this->orderId
+        ]);
+    }
+}
