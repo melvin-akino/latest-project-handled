f5ec32eed5abbe6d2e26e2eb58f681f1d0f5d841
Kevin Uy <kevinuy@ninepinetech.com>
2020-03-19 23:07:39 +0800

    Process - Consume MinMax from Kafka
    Removed comments; fixed $userId socket recipient to $fd

--- app/Tasks/TransformKafkaMessageMinMax.php
+++ app/Tasks/TransformKafkaMessageMinMax.php
@@ -15,85 +15,6 @@ class TransformKafkaMessageMinMax extends Task
 
     public function handle()
     {
-        /**
-            {
-                "request_uid": "77f61545-6756-4463-97a1-b7d8b3824cd9",
-                "request_ts": "123456789",
-                "command": "minmax",
-                "sub_command": "transform",
-                "data": [
-                    {
-                        "provider"  : "hg",
-                        "sport"     : 1,
-                        "market_id" : "EOE4044819",
-                        "odds"      : "1.09",
-                        "minimum"   : "50.00",
-                        "maximum"   : "100000.00"
-                    }
-                ]
-            }
-
-            'providers' => [ // key format [providerAlias:strtolower($providerAlias)] => [id = $id, alias = $alias]
-                'size'   => 500,
-                'column' => [
-                    ['name' => 'id',          'type' => \Swoole\Table::TYPE_INT ],
-                    ['name' => 'alias',       'type' => \Swoole\Table::TYPE_STRING, 'size' => 10 ],
-                    ['name' => 'priority',    'type' => \Swoole\Table::TYPE_INT ],
-                    ['name' => 'is_enabled',  'type' => \Swoole\Table::TYPE_INT ],
-                    ['name' => 'currency_id', 'type' => \Swoole\Table::TYPE_INT ],
-                ],
-            ],
-
-            'exchangeRates' => [
-                'size' => 51200,
-                'column' => [ // KEY FORMAT: [from:$from_currency_code:to:$to_currency_code]
-                    [ 'name' => 'default_amount', 'type' => \Swoole\Table::TYPE_FLOAT ],
-                    [ 'name' => 'exchange_rate',  'type' => \Swoole\Table::TYPE_FLOAT ],
-                ],
-            ],
-
-            'minmax' => [
-                'size' => 102400,
-                'column' => [ // KEY FORMAT: [userId:$userId:memUID:$memUID]
-                    [ 'name' => 'id',             'type' => \Swoole\Table::TYPE_INT ],
-                    [ 'name' => 'user_id',        'type' => \Swoole\Table::TYPE_INT ],
-                    [ 'name' => 'sport_id',       'type' => \Swoole\Table::TYPE_INT ],
-                    [ 'name' => 'provider_alias', 'type' => \Swoole\Table::TYPE_STRING, 'size' => 10 ],
-                    [ 'name' => 'bet_identifier', 'type' => \Swoole\Table::TYPE_STRING, 'size' => 50 ],
-                    [ 'name' => 'order_id',       'type' => \Swoole\Table::TYPE_STRING, 'size' => 50 ],
-                    [ 'name' => 'min',            'type' => \Swoole\Table::TYPE_FLOAT ],
-                    [ 'name' => 'max',            'type' => \Swoole\Table::TYPE_FLOAT ],
-                ],
-            ],
-
-            minMaxData: [
-                {
-                    provider_id: 1,
-                    provider: 'HG',
-                    min: 100,
-                    max: 500,
-                    price: 0.69,
-                    priority: 1
-                },
-                {
-                    provider_id: 2,
-                    provider: 'PIN',
-                    min: 350,
-                    max: 1000,
-                    price: this.odd_details.odds,
-                    priority: 2
-                },
-                {
-                    provider_id: 3,
-                    provider: 'ISN',
-                    min: 600,
-                    max: 2000,
-                    price: this.odd_details.odds,
-                    priority: 3
-                }
-            ]
-        **/
-
         $swoole = app('swoole');
 
         $topics             = $swoole->topicTable;
@@ -105,7 +26,7 @@ class TransformKafkaMessageMinMax extends Task
         $exchangeRatesTable = $swoole->exchangeRatesTable;
 
         $transformed = [];
-        $userId      = "";
+        $fd          = "";
 
         foreach ($minMaxRequests AS $key => $row) {
             foreach ($this->data->data AS $_data) {
