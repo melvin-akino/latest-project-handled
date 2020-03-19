1f3372a2283ed8a7163d83169ca8e998ed2b6b8a
Jan <janroxas@ninepinetech.com>
2020-03-19 19:20:42 +0800

    readabilty

--- app/Models/UserWallet.php
+++ app/Models/UserWallet.php
@@ -8,10 +8,7 @@ class UserWallet extends Model
 {
     protected $table = "wallet";
 
-    public static function getWallet($user_id){
-
-        return self::where('user_id', $user_id);
- 	}
+   
  	public function Order(){
  	
  		 return $this->hasMany('App\Models\Order','user_id','user_id');
