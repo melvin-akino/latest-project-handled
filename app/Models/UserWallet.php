66fe27bf292f08c1b734505fa747da093e5e226e
Jan <janroxas@ninepinetech.com>
2020-03-19 19:23:52 +0800

    readability

--- app/Models/UserWallet.php
+++ app/Models/UserWallet.php
@@ -9,8 +9,10 @@ class UserWallet extends Model
     protected $table = "wallet";
 
    
- 	public function Order(){
- 	
- 		 return $this->hasMany('App\Models\Order','user_id','user_id');
- 	}
+	public function Order() {
+
+	 	return $this->hasMany('App\Models\Order','user_id','user_id');
+
+	}
+
 }
