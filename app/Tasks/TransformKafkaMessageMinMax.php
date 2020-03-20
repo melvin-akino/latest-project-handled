e08aff9a8ea31ab3cc61a03f1d7e026762cb0f74
Alex <johnvirtucio@ninepinetech.com>
2020-03-20 13:21:57 +0800

    Updated minmax

--- app/Tasks/TransformKafkaMessageMinMax.php
+++ app/Tasks/TransformKafkaMessageMinMax.php
@@ -29,78 +29,78 @@ class TransformKafkaMessageMinMax extends Task
         $fd          = "";
 
         foreach ($minMaxRequests AS $key => $row) {
-            foreach ($this->data->data AS $_data) {
-                if ($row['market_id'] == $_data->market_id) {
-                    $memUID = substr($key, strlen('memUID:'));
+            $data = $this->data->data;
+            if ($row['market_id'] == $data->market_id) {
+                $memUID = substr($key, strlen('memUID:'));
 
-                    foreach ($topics AS $_key => $_row) {
-                        if (strpos($_row['topic_name'], 'min-max-' . $memUID) === 0) {
-                            $userId = explode(':', $_key)[1];
-                            $fd     = $wsTable->get('uid:' . $userId);
+                foreach ($topics AS $_key => $_row) {
+                    if (strpos($_row['topic_name'], 'min-max-' . $memUID) === 0) {
+                        $userId = explode(':', $_key)[1];
+                        $fd     = $wsTable->get('uid:' . $userId);
 
-                            /** AS DEFAULT */
-                            $providerCurrency = [
-                                'id'   => 1,
-                                'code' => "CNY",
-                            ];
-                            $providerSwtId    = "providerAlias:" . $_data->provider;
+                        /** AS DEFAULT */
+                        $providerCurrency = [
+                            'id'   => 1,
+                            'code' => "CNY",
+                        ];
+                        $providerSwtId    = "providerAlias:" . $data->provider;
 
-                            if ($provTable->exists($providerSwtId)) {
-                                $providerCurrency['id'] = $provTable->get($providerSwtId)['currency_id'];
-                            }
-
-                            $userCurrency = [
-                                'id'   => 1,
-                                'code' => "CNY",
-                            ];
-                            $userSwtId    = "userId:" . $userId;
-
-                            if ($usersTable->exists($userSwtId)) {
-                                $userCurrency['id'] = $usersTable->get($userSwtId)['currency_id'];
-                            }
-
-                            $transformed[] = [
-                                "sport_id"    => $_data->sport_id,
-                                "provider_id" => $provTable->get($providerSwtId)['id'],
-                                "provider"    => strtoupper($_data->provider),
-                                "min"         => $_data->minimum,
-                                "max"         => $_data->maximum,
-                                "price"       => $_data->odds,
-                                "priority"    => $provTable->get($providerSwtId)['priority'],
-                            ];
-
-                            if (!$providerCurrency['id'] == $userCurrency['id']) {
-                                foreach ($currenciesTable AS $currencyKey => $currencyRow) {
-                                    if (strpos($currencyKey, 'currencyId:' . $userCurrency['id']) === 0) {
-                                        $userCurrency['code'] = $currenciesTable->get($currencyKey)['code'];
-                                    }
-
-                                    if (strpos($currencyKey, 'currencyId:' . $providerCurrency['id']) === 0) {
-                                        $providerCurrency['code'] = $currenciesTable->get($currencyKey)['code'];
-                                    }
-                                }
-
-                                $erSwtId = implode(':', [
-                                    "from:" . $userCurrency['code'],
-                                    "to:"   . $providerCurrency['code'],
-                                ]);
-
-                                if ($exchangeRatesTable->exists($erSwtId)) {
-                                    $exchangeRate = $exchangeRatesTable->get($erSwtId)['exchange_rate'];
-                                }
-
-                                $transformed['min'] = $_data->minimum / $exchangeRate;
-                                $transformed['max'] = $_data->maximum / $exchangeRate;
-                            }
+                        if ($provTable->exists($providerSwtId)) {
+                            $providerCurrency['id'] = $provTable->get($providerSwtId)['currency_id'];
                         }
+
+                        $userCurrency = [
+                            'id'   => 1,
+                            'code' => "CNY",
+                        ];
+                        $userSwtId    = "userId:" . $userId;
+
+                        if ($usersTable->exists($userSwtId)) {
+                            $userCurrency['id'] = $usersTable->get($userSwtId)['currency_id'];
+                        }
+
+                        $transformed = [
+                            "sport_id"    => $data->sport,
+                            "provider_id" => $provTable->get($providerSwtId)['id'],
+                            "provider"    => strtoupper($data->provider),
+                            "min"         => $data->minimum,
+                            "max"         => $data->maximum,
+                            "price"       => $data->odds,
+                            "priority"    => $provTable->get($providerSwtId)['priority'],
+                        ];
+
+                        if (!$providerCurrency['id'] == $userCurrency['id']) {
+                            foreach ($currenciesTable AS $currencyKey => $currencyRow) {
+                                if (strpos($currencyKey, 'currencyId:' . $userCurrency['id']) === 0) {
+                                    $userCurrency['code'] = $currenciesTable->get($currencyKey)['code'];
+                                }
+
+                                if (strpos($currencyKey, 'currencyId:' . $providerCurrency['id']) === 0) {
+                                    $providerCurrency['code'] = $currenciesTable->get($currencyKey)['code'];
+                                }
+                            }
+
+                            $erSwtId = implode(':', [
+                                "from:" . $userCurrency['code'],
+                                "to:"   . $providerCurrency['code'],
+                            ]);
+
+                            if ($exchangeRatesTable->exists($erSwtId)) {
+                                $exchangeRate = $exchangeRatesTable->get($erSwtId)['exchange_rate'];
+                            }
+
+                            
+                            $transformed['min'] = $data->minimum / $exchangeRate;
+                            $transformed['max'] = $data->maximum / $exchangeRate;
+                        }
+
+                        $swoole->push($fd['value'], json_encode([
+                            'getMinMax' => $transformed
+                        ]));
                     }
                 }
             }
         }
-
-        $swoole->push($fd['value'], json_encode([
-            'getMinMax' => $transformed
-        ]));
     }
 
     private static function sortArrayByKey($array, $order, $key)
