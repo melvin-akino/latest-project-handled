6dbbadae05e1c143e80518ff512f5c1c9f71f762
Vin Aquino <melvinaquino@ninepinetech.com>
2020-03-25 17:49:49 +0800

    Created a migration for provider accounts table associated with providers table as its parent

---
+++ database/migrations/2020_03_22_003335_create_provider_accounts.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProviderAccounts extends Migration
+{
+    protected $tablename = "provider_accounts";
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tablename, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('provider_id');
+            $table->integer('type');
+            $table->string('username');
+            $table->string('password');
+            $table->float('punter_percentage', 2, 2);
+            $table->float('credits', 15, 2);
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('provider_id')
+                ->references('id')
+                ->on('providers')
+                ->onUpdate('cascade');
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablename);
+    }
+}
