50c3c6b204cafdd74ec2a12bc424905d9987c0a5
Kevin Uy <kevinuy@ninepinetech.com>
2020-04-14 11:41:45 +0800

    Hotfix
    FIxed incomplete parameters: place bet push to kafka

--- app/Processes/KafkaProduce.php
+++ app/Processes/KafkaProduce.php
@@ -122,8 +122,9 @@ class KafkaProduce implements CustomProcessInterface
                         if (strpos($pKey, 'place-bet-') === 0) {
                             $payload   = json_decode($pRow['payload']);
                             $requestId = $payload->request_uid;
+                            $provider  = $payload->data->provider;
 
-                            self::pushToKafka((array) $payload, $requestId, $kafkaTopics['req_order']);
+                            self::pushToKafka((array) $payload, $requestId, $provider . $kafkaTopics['req_order']);
                         }
 
                         $payloadsTable->del($pKey);
