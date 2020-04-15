18fe039ddabf73aa04608951244cdd50ef5a1892
Alex <johnvirtucio@ninepinetech.com>
2020-04-15 10:23:47 +0800

    HOTFIX semicolon

--- app/Tasks/TransformKafkaMessageBet.php
+++ app/Tasks/TransformKafkaMessageBet.php
@@ -42,7 +42,7 @@ class TransformKafkaMessageBet extends Task
                         'topic_name' => 'open-order-' . $this->message->data->bet_id
                     ]);
 
-                    $ordersTable['orderId:' . $orderId]['bet_id'] = $this->message->data->bet_id
+                    $ordersTable['orderId:' . $orderId]['bet_id'] = $this->message->data->bet_id;
                 }                
             }
         }
