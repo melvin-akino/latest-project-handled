068488502412803726fa040d607af60354f4bae2
Alex <johnvirtucio@ninepinetech.com>
2020-04-20 17:35:44 +0800

    HOTFIX 30s bet retry

--- app/Processes/KafkaProduce.php
+++ app/Processes/KafkaProduce.php
@@ -152,7 +152,7 @@ class KafkaProduce implements CustomProcessInterface
                             }
                         }
 
-                        if ($newTime->diffInSeconds(Carbon::parse($betInitialTime)) >= 90) {
+                        if ($newTime->diffInSeconds(Carbon::parse($betInitialTime)) >= 30) {
                             foreach ($payloadsTable AS $pKey => $pRow) {
                                 if (strpos($pKey, 'place-bet-') === 0) {
                                     $payload   = json_decode($pRow['payload']);
