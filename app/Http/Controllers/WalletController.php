b3d4a60c8150e623cc9b321fa568704fad17ecb7
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-04-23 16:59:07 +0800

    MV-623
    Open orders incorrect display in wallet fix and overlap of order logs in betslip fix

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -22,8 +22,8 @@ class WalletController extends Controller
         if ($wallet) {
             $balance     = $wallet->balance;
             $profit_loss = $wallet->Order()->sum('profit_loss');
-            $orders      = $wallet->Order()->whereNotNull('settled_date')->sum('stake');
-         }   
+            $orders      = $wallet->Order()->where('settled_date', '')->orWhereNull('settled_date')->whereIn('status', ['PENDING', 'SUCCESS'])->sum('stake');
+        }
         return response()->json([
             'status'      => true,
             'status_code' => 200,
