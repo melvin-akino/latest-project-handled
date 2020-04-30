16a44143ffd6857583e581a12076ee7c9dc1adb0
Jan <janroxas@ninepinetech.com>
2020-04-30 18:23:07 +0800

    validating rules and adjustment

--- app/Http/Controllers/PrometheusController.php
+++ app/Http/Controllers/PrometheusController.php
@@ -13,8 +13,63 @@ use Prometheus;
 
 class PrometheusController extends Controller
 {
+
+    public function index() {
+
+        try {
+            $adapter = new \Prometheus\Storage\Redis();
+            $registry = new CollectorRegistry($adapter);
+            $renderer = new RenderTextFormat();
+
+             $keys = Redis::smembers("PROMETHEUS_gauge_METRIC_KEYS");
+             $gauge = array();
+
+             foreach ($keys as $key) {
+                //echo $key .'--<br>';
+                 $raw = Redis::hgetall($key);
+                 //var_dump($raw);
+                 $gauge['samples'] = array();
+                 if(array_key_exists('__meta', $raw)) {
+
+                    $gauge = json_decode($raw['__meta'], true);
+                    unset($raw['__meta']);
+                    foreach ($raw as $k => $value) {
+                        $gauge['samples'][] = array(
+                            'name' => $gauge['name'],
+                            'labelNames' => array(),
+                            'labelValues' => json_decode($k, true),
+                            'value' => $value
+                        );
+                    }
+                    //print_r($gauge);
+
+                    usort($gauge['samples'], function($a, $b){
+                      //  return strcmp(implode("", $a['labelValues']), implode("", $b['labelValues']));
+                        return $a['labelValues'] <=> $b['labelValues'];
+                        //return implode("", $a['labelValues']) <=> implode("", $b['labelValues']);
+                    });
+                    $gauges[] = $gauge;
+
+                }
+
+            }
+            $test = array_map(
+                function (array $metric) {
+                    return new MetricFamilySamples($metric);
+                },
+                $gauges
+            );
+            $result = $renderer->render($test);
+            return response($result)
+                ->header('Content-Type', RenderTextFormat::MIME_TYPE);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            echo  $e->getLine();
+        }
+
+    }
    
-    public function index()
+    public function index___()
     {
         try {
 
@@ -45,7 +100,7 @@ class PrometheusController extends Controller
             $gauges = array();
             foreach ($keys as $key) {
                 //$raw = $this->redis->hGetAll($key);
-                echo $key .'<br>';
+                echo $key .'---<br>';
                 $raw = Redis::hgetall($key);
                 $gauge = json_decode($raw['__meta'], true);
                 echo 'dumping gauge';
@@ -63,6 +118,7 @@ class PrometheusController extends Controller
                 }
                 usort($gauge['samples'], function($a, $b){
                     return strcmp(implode("", $a['labelValues']), implode("", $b['labelValues']));
+
                 });
                 $gauges[] = $gauge;
             }
