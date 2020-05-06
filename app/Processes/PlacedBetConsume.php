38944c723855e161e8021449d5e8a1a277cd04cf
Alex <johnvirtucio@ninepinetech.com>
2020-05-06 10:23:05 +0800

    MV-637 Separation of kafka message to consume

---
+++ app/Processes/PlacedBetConsume.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Processes;
+
+use App\Tasks\TransformKafkaMessageBet;
+use Hhxsv5\LaravelS\Swoole\Process\CustomProcessInterface;
+use Hhxsv5\LaravelS\Swoole\Task\Task;
+use Illuminate\Support\Facades\Log;
+use Swoole\Http\Server;
+use Swoole\Process;
+use Exception;
+
+class PlacedBetConsume implements CustomProcessInterface
+{
+    /**
+     * @var bool Quit tag for Reload updates
+     */
+    private static $quit = false;
+
+    public static function callback(Server $swoole, Process $process)
+    {
+        try {
+            if ($swoole->data2SwtTable->exist('data2Swt')) {
+                $kafkaConsumer = resolve('KafkaConsumer');
+                $kafkaConsumer->subscribe([env('KAFKA_BET_PLACED', 'PLACED-BET')]);
+
+                Log::info("Bet Consume Starts");
+                while (!self::$quit) {
+                    $message = $kafkaConsumer->consume(0);
+                    if ($message->err == RD_KAFKA_RESP_ERR_NO_ERROR) {
+                        $payload = json_decode($message->payload);
+
+                        if (empty($payload->data->status) || empty($payload->data->odds)) {
+                            Log::info("Bet Transformation ignored - Status Or Odds Not Found");
+                            $kafkaConsumer->commitAsync($message);
+                            Log::channel('kafkalog')->info(json_encode($message));
+                            continue;
+                        }
+
+                        Task::deliver(new TransformKafkaMessageBet($payload));
+
+                        $kafkaConsumer->commitAsync($message);
+                        Log::channel('kafkalog')->info(json_encode($message));
+                    }
+                    usleep(10000);
+                }
+            }
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+    }
+
+    // Requirements: LaravelS >= v3.4.0 & callback() must be async non-blocking program.
+    public static function onReload(Server $swoole, Process $process)
+    {
+        self::$quit = true;
+    }
+}
