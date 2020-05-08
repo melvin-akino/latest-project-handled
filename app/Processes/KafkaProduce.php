3997a41aefc21ab5d72767df0aca3ede0542e7c2
Jan <janroxas@ninepinetech.com>
2020-05-08 12:11:22 +0800

    update

--- app/Processes/KafkaProduce.php
+++ app/Processes/KafkaProduce.php
@@ -98,7 +98,7 @@ class KafkaProduce implements CustomProcessInterface
                                 ];
                                 $payload['data'] = $minMaxRequest;
                                
-                                PrometheusMatric::MakeMatrix('request_market_id_total', 'Min-max  total number of  market id  pushed .',$minMaxRequest['market_id']);
+                                \PrometheusMatric::MakeMatrix('request_market_id_total', 'Min-max  total number of  market id  pushed .',$minMaxRequest['market_id']);
                                 self::pushToKafka($payload, $requestId,
                                     strtolower($minMaxRequest['provider']) . $kafkaTopics['req_minmax']);
                             }
