51f1c4a370d24d9e2fafd3c768e67c62aeb68d0c
Alex <johnvirtucio@ninepinetech.com>
2020-05-08 23:01:29 +0800

    HOTFIX trade window events points fix

--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -413,6 +413,7 @@ class TradeController extends Controller
 
                 $transformed = $transformed->whereNull('me.deleted_at')
                     ->where('mem.is_main', true)
+                    ->where('sl.game_schedule', 'em.game_schedule')
                     ->select('ml.sport_id', 'ml.master_league_name', 's.sport', //'mll.provider_id',
                         'me.master_event_unique_id', 'me.master_home_team_name', 'me.master_away_team_name',
                         'me.ref_schedule', 'me.game_schedule', 'me.score', 'me.running_time',
@@ -576,6 +577,7 @@ class TradeController extends Controller
                 ->whereNull('me.deleted_at')
                 ->where('mem.is_main', false)
                 ->where('me.master_event_unique_id', $memUID)
+                ->where('me.game_schedule', 'em.game_schedule')
                 ->select('s.sport',
                     'me.master_event_unique_id', 'me.master_home_team_name', 'me.master_away_team_name',
                     'me.ref_schedule', 'me.game_schedule', 'me.score', 'me.running_time',
