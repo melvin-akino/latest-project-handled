96120759579c89b66d9ffd88bf5aad2a89a6d055
Jan <janroxas@ninepinetech.com>
2020-05-12 19:26:57 +0800

    test

--- app/Jobs/TransformKafkaMessageOdds.php
+++ app/Jobs/TransformKafkaMessageOdds.php
@@ -11,7 +11,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Exception;
-//use Swoole\Http\Server;
+use Swoole\Http\Server;
 //use Swoole\Process;
 
 class TransformKafkaMessageOdds implements ShouldQueue
@@ -52,7 +52,7 @@ class TransformKafkaMessageOdds implements ShouldQueue
         'TEST'
     ];
 
-    public function __construct($message, $swoole)
+    public function __construct($message, Server $swoole)
     {
         $this->message = $message;
         $this->swoole = $swoole;
