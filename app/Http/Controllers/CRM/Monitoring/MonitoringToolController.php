171bd21e9d650103b50b00f8663f3a397721a7be
Jan <janroxas@ninepinetech.com>
2020-05-18 18:44:10 +0800

    added placed bet tools

---
+++ app/Http/Controllers/CRM/Monitoring/MonitoringToolController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\CRM\Monitoring;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+class MonitoringToolController extends Controller
+{
+    //
+
+    public function minmax() 
+    {
+    	$markets = [];
+    	$redisTopic = env('REDIS_TOOL_MINMAX', 'REDIS-MON-TOOL-MINMAX-ODDS');
+    	$smembers = Redis::smembers($redisTopic);
+
+    	if (count($smembers) != 0) {
+	    	foreach ($smembers as $k => $v) {
+	    		$latest = Redis::hget($v,'latest');
+	    		$prev = Redis::hget($v,'previous');
+	    		$markets[$v] = ['previous' => ($prev), 'latest' => ($latest)];
+	    	}
+    	}
+
+    	$data['page_title']       = "Monitoring Tool";
+        $data['monitoring_menu']    = true;
+        $data['page_description'] = 'Minmax Monitoring Tools';
+    	$data['minmaxs'] =  $markets;
+    	return view('CRM.monitoring.minmax')->with($data);
+    }
+
+    public function placedBet()
+    {
+    	$markets = [];
+    	$redisTopic = env('REDIS_TOOL_PLACED_BET', 'REDIS-MON-TOOL-PLACED-BET');
+    	$smembers = Redis::smembers($redisTopic);
+
+    	if (count($smembers) != 0) {
+	    	foreach ($smembers as $k => $v) {
+	    		$latest = Redis::hget($v,'latest');
+	    		$prev = Redis::hget($v,'previous');
+	    		$markets[$v] = ['previous' => ($prev), 'latest' => ($latest)];
+	    	}
+    	}
+
+    	$data['page_title']       = "Monitoring Tool";
+        $data['monitoring_menu']    = true;
+        $data['page_description'] = 'Placed Bet Monitoring Tools';
+    	$data['minmaxs'] =  $markets;
+    	return view('CRM.monitoring.placedbet')->with($data);
+
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -86,6 +86,7 @@ Route::get('/prometheus_GGT8', 'PrometheusController@index');
         Route::namespace('Monitoring')->group(function() {
         	Route::prefix('monitoring')->group(function() {
         		Route::get('minmax', 'MonitoringToolController@minmax')->name('monitoring.minmax');
+        		Route::get('placedbet', 'MonitoringToolController@placedBet')->name('monitoring.placedBet');
         	});
         });
     });
