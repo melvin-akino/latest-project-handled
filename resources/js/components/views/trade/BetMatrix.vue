84e21c45b1f590d5a85de3c10ca05b440909b6c4
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-05-20 00:43:41 +0800

    MV-672
    Bet matrix gets updated on change of points

--- resources/js/components/views/trade/BetMatrix.vue
+++ resources/js/components/views/trade/BetMatrix.vue
@@ -2,9 +2,19 @@
     <div class="betMatrix text-sm">
         <dialog-drag title="Bet Matrix" :options="options" @close="$emit('close')" v-bet-matrix="activeBetSlip==market_id">
             <div class="p-6">
-                <p class="text-gray-700 mb-2">Current Score: {{analysisData.home_score}} - {{analysisData.away_score}}</p>
+                <div class="flex justify-between items-center mb-4">
+                    <p class="text-gray-700">Current Score: {{analysisData.home_score}} - {{analysisData.away_score}}</p>
+                    <div class="relative w-40">
+                        <select class="shadow appearance-none border rounded text-sm w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none" @change="changePoint" v-model="selectedPoint">
+                            <option v-for="(order, index) in matrix_orders" :key="index" :value="order" :selected="order.points">{{order.points}}</option>
+                        </select>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                        </div>
+                    </div>
+                </div>
                 <div class="bg-gray-400 p-2">
-                    <div class="container mx-auto text-sm text-gray-700">Order placed at {{analysisData.created_at}}</div>
+                    <div class="container mx-auto text-sm text-gray-700">Order placed at {{matrix_data.created_at}}</div>
                 </div>
                 <div class="flex items-center bg-black text-white pl-4">
                     <i class="material-icons sportsIcon pr-3">sports_soccer</i>
@@ -24,8 +34,8 @@
                     <span class="w-1/3">Stake</span>
                 </div>
                 <div class="flex items-center text-gray-700 text-white p-1 pl-4">
-                    <span class="w-2/3">{{analysisData.bet_team}} {{analysisData.points}} {{`(${analysisData.price_format})`}}</span>
-                    <span class="w-1/3">{{Number(analysisData.price) | twoDecimalPlacesFormat}}</span>
+                    <span class="w-2/3">{{analysisData.bet_team}} {{matrix_data.points}} {{`(${analysisData.price_format})`}}</span>
+                    <span class="w-1/3">{{matrix_data.price}}</span>
                     <span class="w-1/3">{{analysisData.currency_symbol}} {{matrix_data.stake | moneyFormat}}</span>
                 </div>
             </div>
@@ -36,6 +46,7 @@
 <script>
 import { mapState } from 'vuex'
 import DialogDrag from 'vue-dialog-drag'
+import Cookies from 'js-cookie'
 import { twoDecimalPlacesFormat, convertPointAsNumeric, moneyFormat } from '../../../helpers/numberFormat'
 
 export default {
@@ -55,8 +66,11 @@ export default {
                 price: Number(this.analysisData.price),
                 home_score: Number(this.analysisData.home_score),
                 away_score: Number(this.analysisData.away_score),
-                points: convertPointAsNumeric(this.analysisData.points, this.analysisData.odd_type)
-            }
+                points: convertPointAsNumeric(this.analysisData.points, this.analysisData.odd_type),
+                created_at: this.analysisData.created_at
+            },
+            matrix_orders: [],
+            selectedPoint: {}
         }
     },
     computed: {
@@ -78,9 +92,22 @@ export default {
         }
     },
     mounted() {
+        this.getBetMatrixOrders()
         this.generateBetMatrix()
     },
     methods: {
+        getBetMatrixOrders() {
+            let token = Cookies.get('mltoken')
+
+            axios.get(`v1/orders/bet-matrix/${this.market_id}`, { headers: { 'Authorization': `Bearer ${token}` }})
+            .then(response => {
+                this.matrix_orders = response.data.data
+                this.selectedPoint = response.data.data.filter(order => order.points === this.analysisData.points && order.created_at === this.analysisData.created_at)[0]
+            })
+            .catch(err => {
+                this.$store.dispatch('auth/checkIfTokenIsValid', err.response.data.status_code)
+            })
+        },
         generateBetMatrix() {
             var bet_team_counter = 0;
             while(bet_team_counter <= 10) {
@@ -118,6 +145,14 @@ export default {
                 this.matrix_table.push(table)
                 bet_team_counter++
             }
+        },
+        changePoint() {
+            this.matrix_table = []
+            this.matrix_data.stake = Number(twoDecimalPlacesFormat(this.selectedPoint.stake))
+            this.matrix_data.price = Number(this.selectedPoint.odds)
+            this.matrix_data.points = Number(this.selectedPoint.points)
+            this.matrix_data.created_at = this.selectedPoint.created_at
+            this.generateBetMatrix()
         }
     },
     directives: {
