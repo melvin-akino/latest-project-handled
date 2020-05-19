a6f4d56b116d75ad4fdc42b9904e6c87edb8912a
Jan <janroxas@ninepinetech.com>
2020-05-19 17:54:03 +0800

    nit alingnment

--- app/Http/Controllers/CRM/Monitoring/MonitoringToolController.php
+++ app/Http/Controllers/CRM/Monitoring/MonitoringToolController.php
@@ -14,9 +14,9 @@ class MonitoringToolController extends Controller
 
     public function minmax() 
     {
-    	$markets    = [];
-    	$redisTopic = env('REDIS_TOOL_MINMAX', 'REDIS-MON-TOOL-MINMAX-ODDS');
-    	$smembers   = Redis::smembers($redisTopic);
+    	$markets       = [];
+    	$redisTopic    = env('REDIS_TOOL_MINMAX', 'REDIS-MON-TOOL-MINMAX-ODDS');
+    	$smembers      = Redis::smembers($redisTopic);
 
     	if (count($smembers) != 0) {
 	    	foreach ($smembers as $k => $v) {
@@ -26,22 +26,22 @@ class MonitoringToolController extends Controller
 	    	}
     	}
 
-    	$data['page_title']       	= "Min-max stake Monitoring";
-        $data['page_description'] 	= 'Lists all min-max requests';
-    	$data['minmaxs'] 			=  $markets;
-    	$data['monitoring_menu']    = true;
-    	$data['logs_menu']			= false;
-    	$data['minmax_menu'] 		= true;
-    	$data['placebet_menu'] 		= false;
+        $data['page_title']         = "Min-max stake Monitoring";
+        $data['page_description']   = 'Lists all min-max requests';
+        $data['minmaxs']            =  $markets;
+        $data['monitoring_menu']    = true;
+        $data['logs_menu']          = false;
+        $data['minmax_menu']        = true;
+        $data['placebet_menu']      = false;
  
     	return view('CRM.monitoring.minmax')->with($data);
     }
 
     public function placedBet()
     {
-    	$markets 	= [];
-    	$redisTopic = env('REDIS_TOOL_PLACED_BET', 'REDIS-MON-TOOL-PLACED-BET');
-    	$smembers 	= Redis::smembers($redisTopic);
+    	$markets       = [];
+    	$redisTopic    = env('REDIS_TOOL_PLACED_BET', 'REDIS-MON-TOOL-PLACED-BET');
+    	$smembers      = Redis::smembers($redisTopic);
 
     	if (count($smembers) != 0) {
 	    	foreach ($smembers as $k => $v) {
@@ -51,13 +51,13 @@ class MonitoringToolController extends Controller
 	    	}
     	}
 
-    	$data['page_title']       	= "Bet Monitoring";
-        $data['page_description'] 	= 'Lists all placed bets';
-    	$data['minmaxs'] 			=  $markets;
-    	$data['monitoring_menu']    = true;
-    	$data['logs_menu']			= false;
-    	$data['minmax_menu'] 		= false;
-    	$data['placebet_menu'] 		= true;
+    	$data['page_title']        = "Bet Monitoring";
+        $data['page_description']  = 'Lists all placed bets';
+    	$data['minmaxs']           = $markets;
+    	$data['monitoring_menu']   = true;
+    	$data['logs_menu']         = false;
+    	$data['minmax_menu']       = false;
+    	$data['placebet_menu']     = true;
     	return view('CRM.monitoring.placedbet')->with($data);
 
 
@@ -69,9 +69,9 @@ class MonitoringToolController extends Controller
             'page_title'        => "Odds Monitoring",
             'page_description'  => "Lists all scraped odds from providers",
             'monitoring_menu'   => true,
-            'logs_menu'			=> true,
-            'minmax_menu'		=> false,
-            'placebet_menu'		=> false,
+            'logs_menu'         => true,
+            'minmax_menu'       => false,
+            'placebet_menu'     => false,
         ];
         $redisTopic = env('REDIS_TOOL_SCRAPE_ODDS','REDIS-SCRAPING-ODDS');
         $results    = Redis::smembers($redisTopic);
