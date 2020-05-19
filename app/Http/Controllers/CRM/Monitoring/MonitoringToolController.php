2fdd9188bef474bb133031caf013d5a737aacd74
Jan <janroxas@ninepinetech.com>
2020-05-19 10:49:42 +0800

    added monitoring tools to menu

--- app/Http/Controllers/CRM/Monitoring/MonitoringToolController.php
+++ app/Http/Controllers/CRM/Monitoring/MonitoringToolController.php
@@ -26,31 +26,38 @@ class MonitoringToolController extends Controller
 	    	}
     	}
 
-    	$data['page_title']       = "Monitoring Tool";
-        $data['monitoring_menu']    = true;
-        $data['page_description'] = 'Minmax Monitoring Tools';
-    	$data['minmaxs'] =  $markets;
+    	$data['page_title']       	= "Monitoring Tool";
+        $data['page_description'] 	= 'Minmax Monitoring Tools';
+    	$data['minmaxs'] 			=  $markets;
+    	$data['monitoring_menu']    = true;
+    	$data['logs_menu']			= false;
+    	$data['minmax_menu'] 		= true;
+    	$data['placebet_menu'] 		= false;
+ 
     	return view('CRM.monitoring.minmax')->with($data);
     }
 
     public function placedBet()
     {
-    	$markets = [];
+    	$markets 	= [];
     	$redisTopic = env('REDIS_TOOL_PLACED_BET', 'REDIS-MON-TOOL-PLACED-BET');
-    	$smembers = Redis::smembers($redisTopic);
+    	$smembers 	= Redis::smembers($redisTopic);
 
     	if (count($smembers) != 0) {
 	    	foreach ($smembers as $k => $v) {
 	    		$latest = Redis::hget($v,'latest');
-	    		$prev = Redis::hget($v,'previous');
+	    		$prev 	= Redis::hget($v,'previous');
 	    		$markets[$v] = ['previous' => ($prev), 'latest' => ($latest)];
 	    	}
     	}
 
-    	$data['page_title']       = "Monitoring Tool";
-        $data['monitoring_menu']    = true;
-        $data['page_description'] = 'Placed Bet Monitoring Tools';
-    	$data['minmaxs'] =  $markets;
+    	$data['page_title']       	= "Monitoring Tool";
+        $data['page_description'] 	= 'Placed Bet Monitoring Tools';
+    	$data['minmaxs'] 			=  $markets;
+    	$data['monitoring_menu']    = true;
+    	$data['logs_menu']			= false;
+    	$data['minmax_menu'] 		= false;
+    	$data['placebet_menu'] 		= true;
     	return view('CRM.monitoring.placedbet')->with($data);
 
 
@@ -63,13 +70,16 @@ class MonitoringToolController extends Controller
             $data = [
                 'page_title'       => "Odds Monitoring",
                 'page_description' => "Lists all scraped odds from providers",
-                'dashboard_menu'   => true,
+                'monitoring_menu'   => true,
+                'logs_menu'			=> true,
+                'minmax_menu'		=> false,
+                'placebet_menu'		=> false,
             ];
             $results = Redis::smembers("REDIS-SCRAPING-ODDS");
             foreach ($results as $value) {
                 if(Redis::exists($value)) {
-                    $previous = Redis::hget($value,'previous');
-                    $latest = Redis::hget($value,'latest');
+                    $previous = Redis::hget($value, 'previous');
+                    $latest = Redis::hget($value, 'latest');
                     //$jsonResult[] = [json_decode($previous), json_decode($latest)];
                     $previousJSON = json_decode($previous,true);
                     $latestJSON = json_decode($latest,true);
