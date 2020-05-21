fec0a0fe10ec6873d8b8af1b0aaea00a331e3561
Kevin Uy <kevinuy@ninepinetech.com>
2020-05-21 16:18:01 +0800

    Code Structure - Database Schema Adjustments
    Fixed Migration Script

--- database/migrations/2020_05_21_064826_alter_tables__m_v689.php
+++ database/migrations/2020_05_21_064826_alter_tables__m_v689.php
@@ -21,33 +21,23 @@ class AlterTablesMV689 extends Migration
     {
         // EVENT MARKET :: START
             Schema::table($this->em, function (Blueprint $table) {
-                $table->integer('master_event_id')->nullable();
+                $table->integer('master_event_id')->index()->nullable();
+
+                $table->index('game_schedule');
 
                 $table->foreign('master_event_id')
                     ->references('id')
                     ->on('master_events')
                     ->onUpdate('cascade');
             });
-
-            Schema::table($this->em, function (Blueprint $table) {
-                $table->dropForeign([ 'master_event_unique_id' ]);
-            });
         // EVENT MARKET :: END
-
-        // USER SELECTED LEAGUES :: START
-            Schema::table($this->us, function (Blueprint $table) {
-                $table->integer('master_league_id')->nullable();
-
-                $table->foreign('master_league_id')
-                    ->references('id')
-                    ->on('master_leagues')
-                    ->onUpdate('cascade');
-            });
-        // USER SELECTED LEAGUES :: END
 
         // MASTER EVENT MARKETS :: START
             Schema::table($this->mem, function (Blueprint $table) {
-                $table->integer('master_event_id')->nullable();
+                $table->integer('master_event_id')->index()->nullable();
+
+                $table->index('master_event_market_unique_id');
+                $table->index('odd_type_id');
 
                 $table->foreign('master_event_id')
                     ->references('id')
@@ -59,7 +49,7 @@ class AlterTablesMV689 extends Migration
         // MASTER EVENT LINKS :: START
             Schema::table($this->mel, function (Blueprint $table) {
                 $table->dropColumn('master_event_unique_id');
-                $table->integer('master_event_id')->nullable();
+                $table->integer('master_event_id')->index()->nullable();
 
                 $table->foreign('master_event_id')
                     ->references('id')
@@ -71,7 +61,7 @@ class AlterTablesMV689 extends Migration
         // MASTER EVENT MARKET LINKS :: START
             Schema::table($this->meml, function (Blueprint $table) {
                 $table->dropColumn('master_event_market_unique_id');
-                $table->integer('master_event_market_id')->nullable();
+                $table->integer('master_event_market_id')->index()->nullable();
             });
         // MASTER EVENT MARKET LINKS :: END
     }
@@ -86,31 +76,21 @@ class AlterTablesMV689 extends Migration
         // EVENT MARKET :: START
             Schema::table($this->em, function (Blueprint $table) {
                 $table->dropForeign([ 'master_event_id' ]);
+
+                $table->dropIndex($this->em . '_game_schedule_index');
             });
 
             Schema::table($this->em, function (Blueprint $table) {
                 $table->dropColumn('master_event_id');
-
-                $table->foreign('master_event_unique_id')
-                    ->references('master_event_unique_id')
-                    ->on('master_event_links')
-                    ->onUpdate('cascade');
             });
         // EVENT MARKET :: END
-
-        // USER SELECTED LEAGUES :: START
-            Schema::table($this->us, function (Blueprint $table) {
-                $table->dropForeign([ 'master_league_id' ]);
-            });
-
-            Schema::table($this->us, function (Blueprint $table) {
-                $table->dropColumn('master_league_id');
-            });
-        // USER SELECTED LEAGUES :: END
 
         // MASTER EVENT MARKETS :: START
             Schema::table($this->mem, function (Blueprint $table) {
                 $table->dropForeign([ 'master_event_id' ]);
+
+                $table->dropIndex($this->mem . '_master_event_market_unique_id_index');
+                $table->dropIndex($this->mem . '_odd_type_id_index');
             });
 
             Schema::table($this->mem, function (Blueprint $table) {
