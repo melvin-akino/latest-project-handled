0a8d573916610c5b05de2963a5e6896d01d47f5a
Alex <johnvirtucio@ninepinetech.com>
2020-05-21 17:37:38 +0800

    HOTFIX RETURN_STAKE instead of RETURN_BET

--- app/Jobs/WsSettledBets.php
+++ app/Jobs/WsSettledBets.php
@@ -145,7 +145,7 @@ class WsSettledBets implements ShouldQueue
             ->first();
 
         $returnBetSourceId = DB::table('sources')
-            ->where('source_name', 'LIKE', 'RETURN_BET')
+            ->where('source_name', 'LIKE', 'RETURN_STAKE')
             ->first();
 
         DB::beginTransaction();
