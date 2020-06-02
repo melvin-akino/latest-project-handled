6613b634bbc797eca2e590bf5fb2ce4098e9946f
Jan <janroxas@ninepinetech.com>
2020-06-02 10:51:27 +0800

    update

--- database/seeds/SystemConfigurationProviderThreshold.php
+++ database/seeds/SystemConfigurationProviderThreshold.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\SystemConfiguration;
 
 class SystemConfigurationProviderThreshold extends Seeder
 {
@@ -11,18 +12,21 @@ class SystemConfigurationProviderThreshold extends Seeder
      */
     public function run()
     {
-        //
+        
         $thresholds = [
-        	'BET_NORMAL_THRESHOLD' 			   => 4500,
-        	'BET_VIP_THRESHOLD'				   => 4500,
-        	'PROVIDER_THRESHOLD_SEND_EMAIL_TO' => 'janroxas@ninepinetech.com'
+        	'BET_NORMAL_THRESHOLD'				=> 4500,
+        	'BET_VIP_THRESHOLD'					=> 4500,
+        	'PROVIDER_THRESHOLD_SEND_EMAIL_TO'	=> 'janroxas@ninepinetech.com'
 
         ];
         foreach ($thresholds as $key => $value) {
-            DB::table('system_configurations')->insert([
+        	SystemConfiguration::updateOrCreate([
                 'type'  => $key,
+            ], [
                 'value' => $value,
+                
             ]);
+
         }
 
     }
