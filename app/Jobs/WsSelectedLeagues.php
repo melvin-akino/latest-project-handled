1d5338d54006d4b6bf5a0a75b7f155009f2774b9
Gio Rodriguez <giorodriguez@ninepinetech.com>
2020-06-04 13:40:56 +0800

    MV-718
    Selected leagues filtered by most priority enabled provider

--- app/Jobs/WsSelectedLeagues.php
+++ app/Jobs/WsSelectedLeagues.php
@@ -21,9 +21,9 @@ class WsSelectedLeagues implements ShouldQueue
     {
         $server = app('swoole');
         $fd = $server->wsTable->get('uid:' . $this->userId);
-
+        $providerId = getMostPriorityProvider($this->userId);
         $leagues = [];
-        $userSelectedLeagues = UserSelectedLeague::getSelectedLeagueByUserId($this->userId);
+        $userSelectedLeagues = UserSelectedLeague::getSelectedLeagueByUserId($this->userId, $providerId);
         array_map(function($userSelectedLeague) use (&$leagues) {
             $leagues[$userSelectedLeague->game_schedule][] = $userSelectedLeague->master_league_name;
         }, $userSelectedLeagues->toArray());
