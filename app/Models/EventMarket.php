850465adcb3b3ab2f02c42521b4ed29d77ce8bfe
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2020-06-05 20:15:07 +0800

    HOTFIX get event markets based from memuid

--- app/Models/EventMarket.php
+++ app/Models/EventMarket.php
@@ -27,20 +27,12 @@ class EventMarket extends Model
 
     public static function getEventMarketByMemUID(string $memUID)
     {
-        $eventMarket = self::getProviderEventMarketsByMemUID($memUID);
-
-        if (!$eventMarket) {
-            return null;
-        }
-
         return DB::table('event_markets as em')
                 ->join('providers as p', 'p.id', 'em.provider_id')
                 ->join('events as e', 'e.id', 'em.event_id')
                 ->join('master_events as me', 'me.id', 'e.master_event_id')
-                ->where('e.master_event_id', $eventMarket->master_event_id)
-                ->where('em.odd_type_id', $eventMarket->odd_type_id)
-                ->where('em.odd_label', $eventMarket->odd_label)
-                ->where('em.market_flag', $eventMarket->market_flag)
+                ->join('master_event_markets as mem', 'mem.id', 'em.master_event_market_id')
+                ->where('mem.master_event_market_unique_id',$memUID)
                 ->select('em.bet_identifier', 'p.alias', 'e.sport_id', 'me.game_schedule', 'e.event_identifier', 'em.odds')
                 ->distinct()
                 ->get();
