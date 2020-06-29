27fd4dcca2d95ed683e3385f5b33e6d5fef17275
Kevin Uy <kevinuy@ninepinetech.com>
2020-06-29 21:41:59 +0800

    Console - Removal of duplicate Event Market records
    Created a console command, removing duplicate event market records.

---
+++ app/Console/Commands/RemoveDuplicateEventMarket.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\{DB, Log};
+use Illuminate\Console\Command;
+
+use Carbon\Carbon;
+
+class RemoveDuplicateEventMarket extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'markets:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Remove duplicate Event Market records.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->line('Cleaning Event Market Database Tables...');
+
+        $memIdOrders = DB::table('orders')
+            ->pluck('master_event_market_id')
+            ->toArray();
+
+        $memDeleteNotIn = DB::table('master_event_markets')
+            ->whereNotIn('id', $memIDs);
+
+        $emDeleteInMEM = DB::table('event_markets')
+            ->whereIn('master_event_market_id', $memIDs);
+
+        $emDeleteNull = DB::table('event_markets')
+            ->whereNull('master_event_market_id');
+
+        $emDeleteDuplicate = DB::table('event_markets')
+            ->select('master_event_market_id')
+            ->groupBy('master_event_market_id')
+            ->havingRaw('COUNT(*) > 1')
+            ->update([ 'deleted_at' => Carbon::now() ]);
+
+        $memDeleteNotIn->delete();
+        $emDeleteInMEM->delete();
+        $emDeleteNull->delete();
+
+        $this->info('Done!');
+    }
+}
