f949432bd0e1014a31a35e1ce16681a2b023c443
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2020-08-05 19:45:50 +0800

    - additional migration for orders table

---
+++ database/migrations/2020_08_05_095642_add_columns_to_orders_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToOrdersTable extends Migration
+{
+    protected $ordersTable = 'orders';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable($this->ordersTable)) {
+            Schema::table($this->ordersTable, function (Blueprint $table) {
+                $table->enum('market_flag', [
+                    'HOME',
+                    'DRAW',
+                    'AWAY'
+                ])->nullable();
+                $table->integer('odd_type_id')->nullable();
+                $table->string('final_score', 10)->nullable();
+
+                $table->string('master_league_name', 100)->nullable();
+                $table->string('master_team_home_name', 100)->nullable();
+                $table->string('master_team_away_name', 100)->nullable();
+
+                $table->foreign('odd_type_id')
+                      ->references('id')
+                      ->on('odd_types')
+                      ->onUpdate('cascade');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable($this->ordersTable)) {
+            Schema::table($this->ordersTable, function (Blueprint $table) {
+                $table->dropColumn('market_flag');
+                $table->dropColumn('odd_type_id');
+                $table->dropColumn('final_score');
+                $table->dropColumn('master_league_name');
+                $table->dropColumn('master_team_home_name');
+                $table->dropColumn('master_team_away_name');
+            });
+        }
+    }
+}
