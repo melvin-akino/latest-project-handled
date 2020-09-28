8d2e0d066cb0941113cb0f69b3ec5d2eb6393405
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2020-09-28 13:12:46 +0800

    - request created at minus 5hrs aside from current date minus 5hrs

--- app/Processes/SettlementProduce.php
+++ app/Processes/SettlementProduce.php
@@ -56,10 +56,42 @@ class SettlementProduce implements CustomProcessInterface
                                 if ($newTime->diffInSeconds(Carbon::parse($startTime)) >= $systemConfigurationsTimer->value) {
                                     foreach ($providerAccountsTable as $pRow) {
 
-                                        $providerHasUnsettled = Order::where('provider_account_id', $pRow['id'])
-                                                                     ->where('status', 'SUCCESS')
-                                                                     ->exists();
-                                        if ($providerHasUnsettled) {
+                                        $providerUnsettledDates = Order::where('provider_account_id', $pRow['id'])
+                                                                       ->whereNull('settled_date')
+                                                                       ->groupBy('unsettled_date')
+                                                                       ->select(DB::raw('DATE(created_at) as unsettled_date'))
+                                                                       ->pluck('unsettled_date')
+                                                                       ->toArray();
+
+                                        foreach ($providerUnsettledDates as $providerUnsettledDate) {
+                                            $providerAlias = strtolower($pRow['provider_alias']);
+                                            $username      = $pRow['username'];
+                                            $requestId     = (string) Str::uuid();
+                                            $requestTs     = getMilliseconds();
+                                            $payload       = [
+                                                'request_uid' => $requestId,
+                                                'request_ts'  => $requestTs,
+                                                'sub_command' => 'scrape',
+                                                'command'     => 'settlement'
+                                            ];
+
+                                            $payload['data'] = [
+                                                'sport'           => $sportId,
+                                                'provider'        => $providerAlias,
+                                                'username'        => $username,
+                                                'settlement_date' => Carbon::createFromFormat('Y-m-d', $providerUnsettledDate)->subHours(5)->format('Y-m-d')
+                                            ];
+
+                                            KafkaPush::dispatch(
+                                                $providerAlias . env('KAFKA_SCRAPE_SETTLEMENT_POSTFIX', '_settlement_req'),
+                                                $payload,
+                                                $requestId
+                                            );
+
+                                            // add sleep to prevent detecting as bot
+                                            usleep(random_int(1, 3) * 1000000);
+                                        }
+                                        if (!empty($providerHasUnsettled)) {
                                             $providerAlias = strtolower($pRow['provider_alias']);
                                             $username      = $pRow['username'];
                                             $requestId     = (string) Str::uuid();
