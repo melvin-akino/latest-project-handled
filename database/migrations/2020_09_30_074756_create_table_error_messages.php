cbc1b63adcd3b3e53f9409beed79b8e741d9e255
Vin Aquino <melvinaquino@ninepinetech.com>
2020-09-30 15:51:00 +0800

    added migration create table error_messages

---
+++ database/migrations/2020_09_30_074756_create_table_error_messages.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableErrorMessages extends Migration
+{
+    protected $tablename = "error_messages";
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tablename, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('error',255)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tablename);
+    }
+}
+/*
+id int autoincrement primary
+error varchar(255) unique
+created_at timestamp
+updated_at timestamp ON Update
+deleted_at datetime null
