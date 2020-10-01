3d367ca14c99ab317e3099e1ca59a20c747c855f
Jan <janroxas@ninepinetech.com>
2020-10-01 12:37:09 +0800

    intiial

---
+++ app/Http/Controllers/CRM/Message/ProviderErrorController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\CRM\Message;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProviderErrorController extends Controller
+{
+    //
+
+    public function index() 
+    {
+
+    }
+
+    public function create() 
+    {
+
+    }
+    public function update() 
+    {
+
+    }
+    public function delete() 
+    {
+
+    }
+}
