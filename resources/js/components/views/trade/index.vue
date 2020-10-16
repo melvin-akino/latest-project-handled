b7319c57692b52577bdd91b0c975285fe8cf52f4
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2020-10-16 18:39:30 +0800

    - this instead of state in VUE

--- resources/js/components/views/trade/index.vue
+++ resources/js/components/views/trade/index.vue
@@ -174,7 +174,7 @@ export default {
                             this.$store.commit('trade/REMOVE_ALL_FROM_EVENT_LIST', { league_name: removedEvent.league_name, game_schedule: removedEvent.game_schedule, uid: removedEvent.uid })
                             let leagueMatchCount = this.eventsList.filter(event => removedEvent.league_name == event.league_name && removedEvent.game_schedule == event.game_schedule && !event.hasOwnProperty('watchlist')).length
                             if(leagueMatchCount == 1) {
-                                this.$store.dispatch('trade/toggleLeague', { action: 'remove', league_name: removedEvent.league_name,  schedule: removedEvent.game_schedule, sport_id: state.selectedSport })
+                                this.$store.dispatch('trade/toggleLeague', { action: 'remove', league_name: removedEvent.league_name,  schedule: removedEvent.game_schedule, sport_id: this.selectedSport })
                                 this.$store.commit('trade/REMOVE_SELECTED_LEAGUE', {schedule: removedEvent.game_schedule, league: removedEvent.league_name })
                                 this.$store.commit('trade/REMOVE_FROM_LEAGUE', {schedule: removedEvent.game_schedule, league: removedEvent.league_name })
                             } else {
