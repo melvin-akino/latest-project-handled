1a439bd04e644f1c6e769e9017aa988995f2e840
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2020-10-26 15:56:11 +0800

    - updated removal of events logic

--- resources/js/components/views/trade/index.vue
+++ resources/js/components/views/trade/index.vue
@@ -156,8 +156,10 @@ export default {
                         if(leagueNames.includes(receivedEvents.leagueName)) {
                             let leagueMatchCount = this.leagues[receivedEvents.schedule].filter(league => league.name == receivedEvents.leagueName)[0].match_count
                             if(leagueMatchCount == 1) {
-                                this.$store.dispatch('trade/toggleLeague', { action: 'remove', league_name: receivedEvents.leagueName,  schedule: receivedEvents.schedule, sport_id: receivedEvents.sport_id })
-                                this.$store.commit('trade/REMOVE_SELECTED_LEAGUE', {schedule: receivedEvents.schedule, league: receivedEvents.leagueName })
+                                if(this.selectedLeagues[receivedEvents.schedule].includes(receivedEvents.leagueName)) {
+                                    this.$store.dispatch('trade/toggleLeague', { action: 'remove', league_name: receivedEvents.leagueName,  schedule: receivedEvents.schedule, sport_id: receivedEvents.sport_id })
+                                    this.$store.commit('trade/REMOVE_SELECTED_LEAGUE', {schedule: receivedEvents.schedule, league: receivedEvents.leagueName })
+                                }
                                 this.$store.commit('trade/REMOVE_FROM_LEAGUE', {schedule: receivedEvents.schedule, league: receivedEvents.leagueName })
                             }
                         }
