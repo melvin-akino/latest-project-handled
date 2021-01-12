80df22d9f22bffbbdacd869a63f5729fe24f1dd6
John Alexander Virtucio <johnvirtucio@ninepinetech.com>
2021-01-12 18:41:20 +0800

    - removed unneeded codes

--- app/Http/Middleware/StillActive.php
+++ app/Http/Middleware/StillActive.php
@@ -3,19 +3,14 @@
 namespace App\Http\Middleware;
 
 use App\Facades\SwooleHandler;
-use App\User;
 use Closure;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class StillActive
 {
     public function handle($request, Closure $next)
     {
-        Log::debug($request->path());
-        Log::debug(Auth::user());
-
-        $userStatus = Auth::user()->status;
+        $userStatus   = Auth::user()->status;
         $cachedStatus = SwooleHandler::getValue('userStatusesTable', auth()->user()->id)['status'];
         if ($userStatus != 1 || $cachedStatus != 1) {
             if ($cachedStatus != $userStatus) {
