8666deda05c29b24d21575c011e1a6c16e8c4895
Kevin <kevinuy@ninepinetech.com>
2021-01-29 15:20:47 +0800

    Fixed helper function

--- app/helper.php
+++ app/helper.php
@@ -770,7 +770,7 @@ if (!function_exists('kafkaPush')) {
     }
 }
 
-if (function_exists('monitorLog')) {
+if (!function_exists('monitorLog')) {
     function monitorLog(string $channel, string $level, $data)
     {
         Log::channel($channel)->{$level}(json_encode($data));
