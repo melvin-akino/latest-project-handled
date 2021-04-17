a7f01c909e853d6373fb413ed7344c482e89c763
Kevin Uy <kevinuy@ninepinetech.com>
2021-04-17 18:02:18 +0800

    Save correct mem_uid upon placing bet

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -585,7 +585,7 @@ class OrdersController extends Controller
                 $_orderData = [
                     'master_event_market_id'        => $eventMarket->id,
                     'master_event_unique_id'        => $query->master_event_unique_id,
-                    'master_event_market_unique_id' => $request->market_id,
+                    'master_event_market_unique_id' => $query->mem_uid,
                     'market_id'                     => $query->bet_identifier,
                     'odds'                          => $row['price'],
                     'odd_label'                     => $query->odd_label,
--- app/Models/Game.php
+++ app/Models/Game.php
@@ -106,7 +106,10 @@ class Game extends Model
 
     public static function getMasterEventByMarketId(string $marketId, $providerId)
     {
-        return DB::table('master_events AS me')
+        $event         = DB::table('event_markets')->where('mem_uid', $marketId)->first();
+        $masterEventId = DB::table('event_groups')->select('master_event_id')->where('event_id', $event->event_id)->first();
+        $eventIds      = DB::table('event_groups')->where('master_event_id', $masterEventId->master_event_id)->pluck('event_id');
+        $query         = DB::table('master_events AS me')
             ->leftJoin('master_leagues as ml', 'ml.id', 'me.master_league_id')
             ->leftJoin('league_groups as lg', 'ml.id', 'lg.master_league_id')
             ->leftJoin('leagues as l', 'l.id', 'lg.league_id')
@@ -125,8 +128,8 @@ class Game extends Model
                 $join->on('sot.sport_id', '=', 'me.sport_id');
             })
             ->whereNull('me.deleted_at')
+            ->whereIn('e.id', $eventIds)
             ->where('em.provider_id', $providerId)
-            ->where('em.mem_uid', $marketId)
             ->select([
                 'me.sport_id',
                 'me.master_event_unique_id',
@@ -147,6 +150,8 @@ class Game extends Model
                 'sot.name AS column_type',
             ])
             ->first();
+
+        return $query;
     }
 
     public static function getSelectedLeagueEvents(int $userId, int $sportId)
