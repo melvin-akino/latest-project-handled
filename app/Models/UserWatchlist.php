660ed61a37ffd10a5009dd1878582778e3e79a46
Klane Zurbano <klanezurbano@ninepinetech.com>
2021-05-02 00:10:35 +0800

    :ambulance: MLPRD-687 Remove soft deleted events in userwatchlist match count query

--- app/Models/UserWatchlist.php
+++ app/Models/UserWatchlist.php
@@ -32,9 +32,15 @@ class UserWatchlist extends Model
         $primaryProviderId = Provider::getIdFromAlias(SystemConfiguration::getSystemConfigurationValue('PRIMARY_PROVIDER')->value);
 
         return DB::table('user_watchlist as uw')
-                ->join('master_events as me', 'me.id', 'uw.master_event_id')
+                ->join('master_events as me', function($join) {
+                    $join->on('me.id', 'uw.master_event_id')
+                        ->whereNull('me.deleted_at');
+                })
                 ->join('event_groups as eg', 'eg.master_event_id', 'me.id')
-                ->join('events as e', 'e.id', 'eg.event_id')
+                ->join('events as e', function($join) {
+                    $join->on('e.id', 'eg.event_id')
+                        ->whereNull('e.deleted_at');
+                })
                 ->groupBy('master_league_id', 'user_id', 'e.game_schedule', 'provider_id')
                 ->having('e.provider_id', $primaryProviderId)
                 ->select('master_league_id', DB::raw('count(master_league_id) as match_count'), 'user_id', 'game_schedule', 'provider_id')
