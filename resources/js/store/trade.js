6bd399821111437951609a9dc5c3ffd7e03237bc
Gio Rodriguez <giorodriguez@ninepinetech.com>
2021-08-06 15:12:20 +0800

    Catch if market odds or odd type is in the response on open of betslip in betbar

--- resources/js/store/trade.js
+++ resources/js/store/trade.js
@@ -184,6 +184,7 @@ const mutations = {
                     Vue.set(event.home, 'score', newEvent.home.score)
                     Vue.set(event.away, 'score', newEvent.away.score)
                     Vue.set(event, 'running_time', newEvent.running_time)
+                    Vue.set(event, 'with_providers', newEvent.with_providers)
                     if(event.hasOwnProperty('market_odds') && newEvent.hasOwnProperty('market_odds')) {
                         Vue.set(event.market_odds, 'main', newEvent.market_odds.main)
                         if(newEvent.market_odds.hasOwnProperty('other')) {
@@ -191,6 +192,8 @@ const mutations = {
                         }
                     } else if(!event.hasOwnProperty('market_odds') && newEvent.hasOwnProperty('market_odds')) {
                         Vue.set(event, 'market_odds', newEvent.market_odds)
+                    } else if(event.hasOwnProperty('market_odds') && !newEvent.hasOwnProperty('market_odds')) {
+                        Vue.delete(event, 'market_odds')
                     }
                 }
             })
